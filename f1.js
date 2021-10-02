21a50b6f98dfe8de66a4f224cea590fbaf12e06a
arpan20032009 <dasa20032009@gmail.com>
2021-10-02 16:01:52 +0530

    001

---
+++ f1.js
@@ -0,0 +1,3 @@
+function w1() {
+			document.getElementById("t1").innerHTML = "download the file(you have to right click at the download hyperlink and select save link as),open it then type your number in the bracket ";
+		}
